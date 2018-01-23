56c41f12228da875ecdc8a17fcef1d8060aca628
Mars Wong <marswong618@gmail.com>
2018-01-23 02:40:42 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -42,8 +42,8 @@ class TicTacToe
     !(@board[index].nil? || @board[index] == " ")
   end
 
-  def valid_move?(board, position)
-    position.between?(0, 8) && !position_taken?(board, position)
+  def valid_move?(index)
+    index.between?(0, 8) && !position_taken?(index)
   end
 end
 
